d177801b717e4015a3762fadaaec79d3948ce832
sayiriraviteja-droid <sayiriraviteja@gmail.com>
2026-05-16 17:27:55 +0530

    chore: change base path for custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repository-name/', // <-- CRITICAL: Add your exact GitHub repository name here enclosed in slashes
+  base: '/rmr-interiors/', // <-- CRITICAL: Add your exact GitHub repository name here enclosed in slashes
 })
